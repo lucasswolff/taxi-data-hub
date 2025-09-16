1e4d2b28cd334fb2d226cf8dc9918634cf1ef46e
Lucas Wolff <lucaswolff494@gmail.com>
2025-09-16 05:14:42 -0700

    create dbt and python dags

---
+++ dags/dag_dbt_pipeline.py
@@ -0,0 +1,128 @@
+from datetime import datetime
+from airflow.decorators import dag
+from airflow.operators.bash import BashOperator
+from airflow.datasets import Dataset
+
+dataset_raw_taxi = Dataset("file:///raw_data/*.parquet")
+dataset_taxi_location = Dataset("file:///dbt_taxi_data_hub/seeds/seed_taxi_zone.csv")
+dataset_weather = Dataset("file:///weather/*.parquet")
+
+@dag(
+    dag_id="dbt_pipeline",
+    description="Run dbt models with Airflow",
+    schedule=[dataset_raw_taxi, dataset_taxi_location, dataset_weather],
+    start_date=datetime(2025, 9, 11),
+    catchup=False,
+    params={"dbt_full_refresh": False},
+    tags=["dbt", "analytics", "taxi-data-hub"],
+)
+def dbt_pipeline():
+
+    # Run dbt clean to avoid any cached files 
+    dbt_clean = BashOperator(
+        task_id="dbt_clean",
+        bash_command="cd /usr/local/airflow/dbt_taxi_data_hub && dbt clean --profiles-dir ..",
+    )
+
+    # Run dbt deps (to install packages)
+    dbt_deps = BashOperator(
+        task_id="dbt_deps",
+        bash_command="cd /usr/local/airflow/dbt_taxi_data_hub && dbt deps --profiles-dir ..",
+    )
+
+    # Run dbt seed
+    dbt_seed = BashOperator(
+        task_id="dbt_seed",
+        bash_command="cd /usr/local/airflow/dbt_taxi_data_hub && dbt seed --profiles-dir ..",
+    )
+
+    ## Run and test dbt models
+
+    # run src
+    dbt_run_src = BashOperator(
+        task_id="dbt_run_src",
+        bash_command=(
+            "cd /usr/local/airflow/dbt_taxi_data_hub && "
+            "dbt run --select models/src --profiles-dir .. "
+            ),
+    )
+
+    # run stg
+    dbt_run_stg = BashOperator(
+        task_id="dbt_run_stg",
+        bash_command=(
+            "cd /usr/local/airflow/dbt_taxi_data_hub && "
+            "dbt run --select models/stg --profiles-dir .. "
+            ),
+    )
+
+    # run dim
+    dbt_run_dim = BashOperator(
+        task_id="dbt_run_dim",
+        bash_command=(
+            "cd /usr/local/airflow/dbt_taxi_data_hub && "
+            "dbt run --select models/dim --profiles-dir .. "
+            ),
+    )
+
+    # test dim
+    dbt_test_dim = BashOperator(
+        task_id="dbt_test_dim",
+        bash_command=(
+            "cd /usr/local/airflow/dbt_taxi_data_hub && "
+            "dbt test --select models/dim --profiles-dir .. "
+            ),
+    )
+
+    # run int
+    dbt_run_int = BashOperator(
+        task_id="dbt_run_int",
+        bash_command=(
+            "cd /usr/local/airflow/dbt_taxi_data_hub && "
+            "dbt run --select models/int --profiles-dir .. "
+            ),
+    )
+
+    # test int
+    dbt_test_int = BashOperator(
+        task_id="dbt_test_int",
+        bash_command=(
+            "cd /usr/local/airflow/dbt_taxi_data_hub && "
+            "dbt test --select models/int --profiles-dir .. "
+            ),
+    )
+
+    # run fct
+    dbt_run_fct = BashOperator(
+        task_id="dbt_run_fct",
+        bash_command=(
+            "cd /usr/local/airflow/dbt_taxi_data_hub && "
+            "dbt run --select models/fct --profiles-dir .. "
+            "{% if params.dbt_full_refresh %}--full-refresh{% endif %}"
+            ),
+    )
+
+    # test fct
+    dbt_test_fct = BashOperator(
+        task_id="dbt_test_fct",
+        bash_command=(
+            "cd /usr/local/airflow/dbt_taxi_data_hub && "
+            "dbt test --select models/fct --profiles-dir .. "
+            ),
+    )
+
+    # run mart
+    dbt_run_mart = BashOperator(
+        task_id="dbt_run_mart",
+        bash_command=(
+            "cd /usr/local/airflow/dbt_taxi_data_hub && "
+            "dbt run --select models/mart --profiles-dir .. "
+            "{% if params.dbt_full_refresh %}--full-refresh{% endif %}"
+            ),
+    )
+
+    # Task dependencies
+    dbt_clean >> dbt_deps >> dbt_seed >> dbt_run_src >> dbt_run_stg >> dbt_run_dim >> dbt_test_dim >> dbt_run_int >> dbt_test_int >> dbt_run_fct >> dbt_test_fct >> dbt_run_mart
+
+
+dbt_pipeline()
--- scripts/local_ingestion_weather.py
+++ scripts/local_ingestion_weather.py
@@ -4,64 +4,77 @@ import requests_cache
 from retry_requests import retry
 from datetime import datetime
 
-start_date = "2023-12-31"
-end_date = datetime.now().replace(day=1).strftime("%Y-%m-%d")
 
-# Setup the Open-Meteo API client with cache and retry on error
-cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-openmeteo = openmeteo_requests.Client(session = retry_session)
+def download_api_data(start_date, end_date, url):
 
-url = "https://archive-api.open-meteo.com/v1/archive"
+	# Setup the Open-Meteo API client with cache and retry on error
+	cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+	retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+	openmeteo = openmeteo_requests.Client(session = retry_session)
 
-params = {
-	# Queens, Brooklyn, EWM, Bronx, Manhattan, Staten Island, NYC (used for Unknown)
-    # N/A (Outside of NYC) will not be sourced - null when joined
-	"latitude": [40.6815, 40.6501, 40.6971, 40.8499, 40.7834, 40.5623, 40.7143],
-	"longitude": [-73.8365, -73.9496, -74.1756, -73.8664, -73.9663, -74.1399, -74.006],
-	"start_date": start_date,
-	"end_date": end_date,
-	"hourly": ["temperature_2m", "precipitation", "rain", "snowfall"],
-	"timezone": "America/New_York",
-}
+	params = {
+		# Queens, Brooklyn, EWM, Bronx, Manhattan, Staten Island, NYC (used for Unknown)
+		# N/A (Outside of NYC) will not be sourced - null when joined
+		"latitude": [40.6815, 40.6501, 40.6971, 40.8499, 40.7834, 40.5623, 40.7143],
+		"longitude": [-73.8365, -73.9496, -74.1756, -73.8664, -73.9663, -74.1399, -74.006],
+		"start_date": start_date,
+		"end_date": end_date,
+		"hourly": ["temperature_2m", "precipitation", "rain", "snowfall"],
+		"timezone": "America/New_York",
+	}
 
-responses = openmeteo.weather_api(url, params=params)
+	responses = openmeteo.weather_api(url, params=params)
 
-df_weather = None  
-borough_list = ['Queens', 'Brooklyn', 'EWM', 'Bronx', 'Manhattan', 'Staten Island', 'Unknown']
+	df_weather = None  
+	borough_list = ['Queens', 'Brooklyn', 'EWM', 'Bronx', 'Manhattan', 'Staten Island', 'Unknown']
 
-for i, response in enumerate(responses):
-	# Process hourly data. The order of variables needs to be the same as requested.
-	hourly = response.Hourly()
-	hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-	hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
-	hourly_rain = hourly.Variables(2).ValuesAsNumpy()
-	hourly_snowfall = hourly.Variables(3).ValuesAsNumpy()
+	for i, response in enumerate(responses):
+		# Process hourly data. The order of variables needs to be the same as requested.
+		hourly = response.Hourly()
+		hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+		hourly_precipitation = hourly.Variables(1).ValuesAsNumpy()
+		hourly_rain = hourly.Variables(2).ValuesAsNumpy()
+		hourly_snowfall = hourly.Variables(3).ValuesAsNumpy()
 
-	hourly_data = {"datetime": pd.date_range(
-		start = pd.to_datetime(hourly.Time(), unit = "s"),
-		end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
-		freq = pd.Timedelta(seconds = hourly.Interval()),
-		inclusive = "left"
-	)}
-	
-	hourly_data["latitude"] = response.Latitude()
-	hourly_data["longitude"] = response.Longitude()
+		hourly_data = {"datetime": pd.date_range(
+			start = pd.to_datetime(hourly.Time(), unit = "s"),
+			end = pd.to_datetime(hourly.TimeEnd(), unit = "s"),
+			freq = pd.Timedelta(seconds = hourly.Interval()),
+			inclusive = "left"
+		)}
+		
+		hourly_data["latitude"] = response.Latitude()
+		hourly_data["longitude"] = response.Longitude()
 
-	hourly_data["borough"] = borough_list[i]
+		hourly_data["borough"] = borough_list[i]
 
-	hourly_data["temperature_2m"] = hourly_temperature_2m
-	hourly_data["precipitation"] = hourly_precipitation
-	hourly_data["rain"] = hourly_rain
-	hourly_data["snowfall"] = hourly_snowfall
+		hourly_data["temperature_2m"] = hourly_temperature_2m
+		hourly_data["precipitation"] = hourly_precipitation
+		hourly_data["rain"] = hourly_rain
+		hourly_data["snowfall"] = hourly_snowfall
 
-	hourly_dataframe = pd.DataFrame(data = hourly_data)
+		hourly_dataframe = pd.DataFrame(data = hourly_data)
 
-	if df_weather is None:
-		df_weather = hourly_dataframe.copy()
-	else:
-		df_weather = pd.concat([df_weather, hourly_dataframe], ignore_index=True)
+		if df_weather is None:
+			df_weather = hourly_dataframe.copy()
+		else:
+			df_weather = pd.concat([df_weather, hourly_dataframe], ignore_index=True)
 
-df_weather['api_inserted_datetime'] = datetime.now().replace(microsecond=0)
+	df_weather['api_inserted_datetime'] = datetime.now().replace(microsecond=0)
 
-df_weather.to_parquet("raw_data/weather/weather_api.parquet", engine="pyarrow", index=False)
+	df_weather.to_parquet("raw_data/weather/weather_api.parquet", engine="pyarrow", index=False)
+
+def main():
+	url = "https://archive-api.open-meteo.com/v1/archive"
+
+	start_date = '2023-12-31'
+	end_date = datetime.now().replace(day=1).strftime("%Y-%m-%d")
+
+	print(f'Downloading API weather data')
+	download_api_data(start_date, end_date, url)
+
+	print(f'API weather data successfully downloaded')
+        
+               
+if __name__ == '__main__':
+    main()
